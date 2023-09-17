78882f34fe1030fd754ab1ba2bfb5ce632679636
Bram Delisse <80391302+bramdelisse@users.noreply.github.com>
2023-09-18 00:13:20 +0200

    Add Notion tags

--- manual_run.py
+++ manual_run.py
@@ -4,9 +4,10 @@
 import openai
 import os
 import requests
+import json
 
 # import scripts
-from scripts import transcribe, process_transcript, prepare_data_NOTION, create_data_NOTION
+from scripts import transcribe, process_transcript, check_response, prepare_yield_NOTION
 from prompts import prompts
 
 # declaring secrets
@@ -20,39 +21,36 @@ NOTION_DATABASE_ID = os.environ["NOTION_DATABASE_ID"]
 
 ########## DECLARING VARIABLES ################################################
 
-AUDIO_FILE_PATH = "./voicenotes/230831-reflection_daily.mp3"
-TASK = "daily_reflection"
-
+# declaring static variables
 MAX_WHISPER_AUDIO_SIZE = 26262828
 MAX_CONTEXT_4K = 4097
 MAX_CONTEXT_16K = 4097 * 4
+MAX_BLOCK_SIZE = 2000
+
+# declaring dynamic variables
+AUDIO_FILE_PATH = "./to_process/230905-reflection_daily.mp3"
+THOUGHT = "daily_reflection" # "daily_reflection", "blog", "nederlands"
 
 
 ########## THOUGHT PROCESSING #################################################
 
 ####### TRANSCRIPTION #######
 print("Transcription started.")
-transcript = transcribe(TASK, AUDIO_FILE_PATH) # also deals with long audio files
+transcript = transcribe(THOUGHT, AUDIO_FILE_PATH) # also deals with long audio files
 print("Transcription finished!")
 
 
 ### TRANSCRIPT PROCESSING ###
 print("Text processing started.")
-text_response = process_transcript(TASK, transcript)
+text_response = process_transcript(THOUGHT, transcript)
 print("Text processing succes!")
 
 
 ########## NOTION ###########
 # PREPARE RESPONSE FOR NOTION
-content_notion = prepare_data_NOTION(TASK, text_response)
-
-title_notion = content_notion['title_notion']
-summary_notion = content_notion['summary_notion']
-topics_notion = content_notion['topics_notion']
-critical_questions_notion = content_notion['critical_questions_notion']
+content_notion = check_response(text_response)
 
 # NOTION UPLOAD
-MAX_BLOCK_SIZE = 2000
 url = "https://api.notion.com/v1/pages/"
 
 headers =   {
@@ -61,9 +59,8 @@ headers =   {
     "Content-Type": "application/json"
             }
 
-data_database = create_data_NOTION(TASK, NOTION_DATABASE_ID, MAX_BLOCK_SIZE, # deals with transcripts longer then Notion max block size
-                                     title_notion, summary_notion, topics_notion, critical_questions_notion,
-                                     transcript)
+data_database = prepare_yield_NOTION(THOUGHT, NOTION_DATABASE_ID, MAX_BLOCK_SIZE,
+                                     content_notion, transcript)
 
 response_get = requests.post(url, headers=headers, json=data_database)
 print("Processed text uploading succesful!")
--- prompts.py
+++ prompts.py
@@ -2,8 +2,9 @@
 
 
 prompts = {
+
 'daily_reflection': {
-    'whisper_prompt': """This transcript is a 26-year-old man, whose name is Bram. He is performing his daily reflection on his life.""",
+    'whisper_prompt': """This recording is spoken by a 26-year-old man, whose name is Bram. He is performing his daily reflection on his life.""",
     'gpt_prompt': """
 ### Introduction
 You are a helpful assistant, specialized in summarizing and understanding the core of a given message.
@@ -25,7 +26,33 @@ You are a helpful assistant, specialized in summarizing and understanding the co
 ```
 """
 },
-'blog_post': {
+
+'shower': {
+    'whisper_prompt': """This recording is spoken by a 26-year-old man, whose name is Bram. He is listing some random thoughts.""",
+    'gpt_prompt': """
+### Introduction
+You are a helpful assistant, specialized in summarizing and understanding the core of a given message.
+In the following piece of text various different thoughts are stated.
+
+### Tasks
+1. Create a title.
+2. Summarize the content of the text.
+3. Extract potential lessons learned by the writer.
+4. List the topics discussed in the text. Only add a topic when it is discussed in multiple sentences.
+5. Imagine being a critical life coach of the person writing this text. Provide the three best questions you could ask regarding the text.
+
+### Rules
+- Always perform all five tasks.
+- Provide your output in JSON format.
+
+### Output
+```json
+{"title": "[output task 1]", "summary": "[output task 2]", "lessons": "["[output task 3]"]", "topics": ["[output task 4]"], "critical questions": ["[output task 5]"]}
+```
+"""
+},
+
+'blog': {
     'whisper_prompt': """This transcript is a 26-year-old man, whose name is Bram. He is brainstorming about a potential blogpost.""",
     'gpt_prompt': """
 ### Introduction
@@ -44,6 +71,28 @@ Imagine being a blog post writer. You are smart, kind, and humble. You write fac
 ### Output
 ```json
 {"title": "[output task 1]", "summary": "[output task 2]", "blog_post": "[output task 3]"}
+```
+"""
+},
+
+'nederlands': {
+    'whisper_prompt': """Hier volgt een Nederlands gesprek, dat in het Nederlands getranscribeerd moet worden. Een van de deelnemers heet Bram.""",
+    'gpt_prompt': """
+### Introductie
+Je bent een behulpvolle assistent, gespecialiseerd in het samenvatten van tekst.
+
+### Taken
+1. Maak een titel.
+2. Vat de inhoud van de tekst samen.
+3. Som de onderwerpen op die besproken worden in de tekst. Voeg een onderwerp pas toe als het voor minstens meerdere zinnen besproken wordt.
+
+### Regels
+- Voer altijd alle drie de taken uit.
+- Geef de output in een JSON format.
+
+### Output
+```json
+{"title": "[output taak 1]", "samenvatting": "[output taak 2]", "onderwerpen": "["[output taak 3]"]"}
 ```
 """
 }
